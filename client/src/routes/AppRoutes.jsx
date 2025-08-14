02f27ef0e93d570ce1bb34040d44389620562b9a
gouravshaw2014 <gouravshaw2014@gmail.com>
2025-08-14 15:48:04 +0530

    Dropdown multiselect component added with prefernce page

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import OTPVerification from "../pages/OTPVerification";
 import BasicInfo from "../pages/TeacherProfileCreation/BasicInfo";
+import Preference from "../pages/TeacherProfileCreation/Preference";
 // import WelcomeTeacher from "../pages/WelcomeTeacher";
 // import WelcomeParent from "../pages/WelcomeParent";
 // import StepBasic from "../pages/ProfileCreation/StepBasic";
@@ -23,6 +24,7 @@ export default function AppRoutes() {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/otp" element={<OTPVerification />} />
       <Route path="/profile/basic" element={<BasicInfo />} />
+      <Route path="/profile/pref" element={<Preference />} />
       {/* <Route path="/welcome-teacher" element={<WelcomeTeacher />} />
       <Route path="/welcome-parent" element={<WelcomeParent />} />
       <Route path="/profile/basic" element={<StepBasic />} />
